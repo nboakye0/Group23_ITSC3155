f50b3b242dbc6fa3e5698296bccfcd0c6b6138fa
Nanayaw Boakye <nboakye@uncc.edu>
2021-11-11 09:44:23 -0500

    Added database and made form for new question and added some routes

--- app.py
+++ app.py
@@ -4,8 +4,24 @@
 import os
 from flask import Flask  # Flask is the web app that we will customize
 from flask import render_template  # render_template class to render HTML
+from flask import request
+from database import db
+from flask import redirect, url_for  # to redirect and use 'url_for' function
+from models import User as User
+from models import Question as Question
+from models import Answer as Answer
 
 app = Flask(__name__)
+# configure database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///group23.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+#  Bind SQLAlchemy db object to this Flask app
+db.init_app(app)
+
+# Setup models
+with app.app_context():
+    db.create_all()   # run under the app context
 
 @app.route("/")
 @app.route("/index")
@@ -14,4 +30,32 @@ def index():
 
 @app.route("/questions")
 def get_questions():
-    return render_template('questions.html')
+    my_questions = db.session.query(Question).all()
+
+    return render_template('questions.html', notes=my_questions)
+
+@app.route('/questions/new', methods=['GET', 'POST'])
+def new_question():
+    # check request method
+    if request.method == 'POST':
+        # get data needed
+        title = request.form['title']
+        details = request.form['details']
+        # date
+        from datetime import date
+        today = date.today()
+        today = today.strftime("%m-%d-%Y")
+        upvote = 0
+        downvote = 0
+        PIN = 0
+        new_record = Question(title, details, today, upvote, downvote, PIN)
+        db.session.add(new_record)
+        db.session.commit()
+
+        return redirect(url_for('get_notes'))
+    else:
+        return render_template('new.html')
+
+
+
+
---
+++ models.py
@@ -0,0 +1,30 @@
+from database import db
+
+class User(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column("name", db.String(200))
+
+class Question(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    title = db.Column("title", db.String(200))
+    details = db.Column("details", db.String(700))
+    date = db.Column("date", db.String(100))
+    upvote = db.Column("upvote", db.Integer)
+    downvote = db.Column("downvote", db.Integer)
+    PIN = db.Column("pin", db.Integer)   # for boolean 0 or 1
+
+    def __init__(self, title, text, date, upvote, downvote, PIN):
+        self.title = title;
+        self.text = text;
+        self.date = date;
+        self.upvote = upvote;
+        self.downvote = downvote;
+        self.PIN = PIN;
+
+class Answer(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    text = db.Column("text", db.String(700))
+    date = db.Column("date", db.String(100))
+    upvote = db.Column("upvote", db.Integer)
+    downvote = db.Column("downvote", db.Integer)
+    PIN = db.Column("pin", db.Integer)   # for boolean 0 or 1
