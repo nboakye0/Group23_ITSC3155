6ac124357e9b42ea39a2f94904e2d668ac48c8a4
Eliza <edeguzm6@uncc.edu>
2021-11-12 16:29:31 -0500

    added individual question view and edited questions view. fixed url links

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Q & A app file
 
-#imports
+# imports
 import os
 from flask import Flask  # Flask is the web app that we will customize
 from flask import render_template  # render_template class to render HTML
@@ -14,25 +14,35 @@ from models import Answer as Answer
 app = Flask(__name__)
 # configure database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///group23.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #  Bind SQLAlchemy db object to this Flask app
 db.init_app(app)
 
 # Setup models
 with app.app_context():
-    db.create_all()   # run under the app context
+    db.create_all()  # run under the app context
+
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template('index.html')
 
+
 @app.route("/questions")
 def get_questions():
     my_questions = db.session.query(Question).all()
 
-    return render_template('questions.html', notes=my_questions)
+    return render_template('questions.html', questions=my_questions)
+
+# View individual question
+@app.route("/questions/<question_id>")
+def get_question(question_id):
+    my_question = db.session.query(Question).filter_by(id=question_id).one()
+
+    return render_template('question.html', question=my_question)
+
 
 @app.route('/questions/new', methods=['GET', 'POST'])
 def new_question():
@@ -52,10 +62,7 @@ def new_question():
         db.session.add(new_record)
         db.session.commit()
 
-        return redirect(url_for('get_notes'))
+        return redirect(url_for('get_questions'))
     else:
         return render_template('new.html')
-
-
-
 
